4e29f805cc7e0c1d5e624de5c063143674529097
aduncan274 <77519411+alinanonymous@users.noreply.github.com>
2023-09-26 11:59:40 -0500

    Better formatting for strings

--- Main.java
+++ Main.java
@@ -24,7 +24,14 @@ class Main {
 
     public static void main(String[] args) {
         System.out.println("\u001b[2J");
-        type("\u001b[HIt is Autumn of 1972. \nTragedy strikes 19 year old Rose Taylor on a drive home with her boyfriend,\n22 year old Johnny Sawyer. After being abandoned at a gas station by her \nfalse lover, a relaxing night on the town would become a nightmare that was\nall too real.\n      The worst things she could have imagined do not compare to the horrors\nshe would experience during the events of this night and the subsequent days.\nThe case of Rose Taylor's misfortunate suffering and anguish would be\ndocumented as yet another story of the bizarre crimes that were part of:\n\n",
+        type("\u001b[HIt is Autumn of 1972. \n" + 
+        "Tragedy strikes 19 year old Rose Taylor on a drive home with her boyfriend,\n" + 
+        "22 year old Johnny Sawyer. After being abandoned at a gas station by her \n" +
+        "false lover, a relaxing night on the town would become a nightmare that was\n" +
+        "all too real.\n      The worst things she could have imagined do not compare to the horrors\n" +
+        "she would experience during the events of this night and the subsequent days.\n" + 
+        "The case of Rose Taylor's misfortunate suffering and anguish would be\n" +
+        "documented as yet another story of the bizarre crimes that were part of:\n\n",
                 25);
         type("\t\tThe Texas Chain Saw Massacre.\n", 50);
         Scanner input = new Scanner(System.in);
